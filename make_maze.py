0fa9816a98f6b901b9ce7baeb37f3ac49f8768cd
Katsigiannis Fotis <katsigiannis.ac@gmail.com>
2015-03-24 23:27:24 +0200

    Update make_maze.py

--- make_maze.py
+++ make_maze.py
@@ -16,14 +16,4 @@ y = int(sys.argv[3])
 random.seed(sys.argv[4])
 outputfile = sys.argv[5]
 
-def get_matrix(n):
-   """Read in and return a matrix of ints with n rows and n columns"""
-   mat = []
-   for i in range(n):
-      row_str = raw_input("")
-      row_str_l = row_str.split()
-      row = []
-      for j in range(n):
-         row.append(int(row_str_l[j]))
-      mat.append(row)
-   return mat
+graph = [[0 for i in range(n)] for j in range(n)]
